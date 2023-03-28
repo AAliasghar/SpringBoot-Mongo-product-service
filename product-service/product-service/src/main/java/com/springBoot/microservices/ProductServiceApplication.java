c703afb4673c56e33a475889fc94132d9b315b71
AAliasghar <a.asgharinia@yahoo.com>
2023-03-28 22:53:37 +0200

    Service Discovery

--- product-service/product-service/src/main/java/com/springBoot/microservices/ProductServiceApplication.java
+++ product-service/product-service/src/main/java/com/springBoot/microservices/ProductServiceApplication.java
@@ -3,7 +3,10 @@ package com.springBoot.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
