90ba2ddd63b1a3fbc2acaa9397952307326277ed
AAliasghar <a.asgharinia@yahoo.com>
2023-04-17 00:18:25 +0200

    DB Update

--- DB/DB.sql
+++ DB/DB.sql
@@ -4,8 +4,21 @@
 --  DROP SCHEMA IF EXISTS `order-service`;
 
 --  CREATE SCHEMA `order-service`;
- USE `order-service` ;
+	USE `order-service` ;
+-- -----------------------------------------------------
+-- Table `t_order_line_items`
+-- -----------------------------------------------------
 
+ SELECT * FROM `order-service`.t_order_line_items;
+ -- -----------------------------------------------------
+-- Table `t_orders_order_line_items_list;`
 -- -----------------------------------------------------
--- Table `full-stack-employee`.``
+ SELECT * FROM `order-service`.t_orders_order_line_items_list;
+ -- -----------------------------------------------------
+-- Table `t_orders`
 -- -----------------------------------------------------
+ SELECT * FROM `order-service`.t_orders;
+ -- -----------------------------------------------------
+-- Table `t_orders_seq`
+-- -----------------------------------------------------
+SELECT * FROM  `order-service`.t_orders_seq; 
